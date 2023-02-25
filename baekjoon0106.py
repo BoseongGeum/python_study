bea63352084d933088488f29962ce99a64588904
BoseongGeum <123168862+BoseongGeum@users.noreply.github.com>
2023-02-25 22:22:49 +0900

    bj230225

--- baekjoon0106.py
+++ baekjoon0106.py
@@ -3,13 +3,19 @@ input = sys.stdin.readline
 
 n = int(input())
 l = list(map(int, input().split()))
+xi = l[0]
 
-for x in l[1:]:
-    y = l[0] / x
-    m = 1
+for yi in l[1:]:
+    y = yi
+    x = xi
     while True:
-        if y == int(y):
-            break
-        y *= m
-        m += 1
-    print(f'{int(y)}/{m}')
+        if x >= y:
+            x = x % y
+            if x == 0:
+                print(f'{xi//y}/{yi//y}')
+                break
+        else:
+            y = y % x
+            if y == 0:
+                print(f'{xi//x}/{yi//x}')
+                break

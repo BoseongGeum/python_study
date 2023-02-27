231c1e00c1bfda263354f460312aa68c5f792096
BoseongGeum <123168862+BoseongGeum@users.noreply.github.com>
2023-02-27 10:54:48 +0900

    bj230227

--- baekjoon0112.py
+++ baekjoon0112.py
@@ -3,4 +3,24 @@ input = sys.stdin.readline
 
 n,m = map(int, input().split())
 c = 0
-for 
+l = [m, n-m]
+
+i = 1
+while True:
+    if n < 5**i:
+        break
+    c += n // 5**i
+    i += 1
+
+for x in l:
+    i = 1
+    while True:
+        if x < 5**i:
+            break
+        c -= x // 5**i
+        i += 1
+if n == 5:
+    if m == 1 or 4:
+        print(0)
+else:
+    print(c)

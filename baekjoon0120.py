a140c1f5826c2211e7386aefb20272174a35ada9
BoseongGeum <123168862+BoseongGeum@users.noreply.github.com>
2023-03-02 17:55:34 +0900

    bj230302+

--- baekjoon0120.py
+++ baekjoon0120.py
@@ -3,9 +3,19 @@ input = sys.stdin.readline
 sys.setrecursionlimit(10000) 
 
 n, m = map(int, input().split())
-l1 = map(int, input().split())
+l1 = list(map(int, input().split()))
 l2 = []
+l3 = []
 s = 0
+num = 1
 for x in l1:
-    s += x
-    l2.append(s)
+    if len(l2) >= m:
+        s += x
+        l2.append(s)
+        l3.append(l2[-1] - l2[-1 - m])
+    else:
+        s += x
+        l2.append(s)
+        l3.append(s)
+del l3[0:m - 1]
+print(max(l3))

c6f46484e6f7d1a0aca55a3c7b619802e56d6926
BoseongGeum <rmaqhtjd8364@naver.com>
2023-05-29 22:58:42 +0900

    bj230529

---
+++ baekjoon290.py
@@ -0,0 +1,14 @@
+import sys
+input = sys.stdin.readline
+
+n = int(input())
+if n == 1:
+    print(1)
+else:
+    d = [0 for _ in range(n+1)]
+    d[1] = 1
+    d[2] = 2
+    for i in range(3, n+1):
+        d[i] = d[i-1] + d[i-2]
+
+    print(d[n] % 10007)

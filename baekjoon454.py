30e49976e09d969a3ec39c6794f82868b0884263
BoseongGeum <rmaqhtjd8364@naver.com>
2023-11-18 16:20:39 +0900

    bj231118

--- baekjoon454.py
+++ baekjoon454.py
@@ -2,8 +2,8 @@ import sys
 input = sys.stdin.readline
 
 months = [4, 6, 9, 11]
-days = ['SUN', 'MON', 'TUE', 'WED', 'THU', 'FRI', 'SAT']
-month, day = map(int, input().split())
+days = ['Wednesday', 'Thursday', 'Friday', 'Saturday', 'Sunday', 'Monday', 'Tuesday']
+day, month = map(int, input().split())
 
 sumDays = 0
 for cur in range(1, month):

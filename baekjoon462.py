d4aa33dbd09eb21f0b076fd8a8cb8be29be23012
BoseongGeum <rmaqhtjd8364@naver.com>
2023-11-27 22:40:32 +0900

    Update baekjoon462.py

--- baekjoon462.py
+++ baekjoon462.py
@@ -1,10 +1,15 @@
 import sys
 input = sys.stdin.readline
 
-t = int(input())
-for _ in range(t):
-    numDict = {str(k):0 for k in range(1, 10)}
-    n = input().rstrip()
-    for i in n:
-        numDict[i] = 1
-    print(list(numDict.values()).count(1))
+string = input().rstrip()
+
+countJ = 0
+countI = 0
+for i in range(len(string)-2):
+    if string[i:i+3] == 'JOI':
+        countJ += 1
+    elif string[i:i+3] == 'IOI':
+        countI += 1
+
+print(countJ)
+print(countI)

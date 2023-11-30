a026f9c7822e135cda13a3a87faccf2e684b7384
BoseongGeum <rmaqhtjd8364@naver.com>
2023-12-01 03:34:47 +0900

    bj231201

--- baekjoon466.py
+++ baekjoon466.py
@@ -2,11 +2,8 @@ import sys
 input = sys.stdin.readline
 
 n = int(input())
-l_a = sorted(list(map(int, input().split())))
-l_b = sorted(list(map(int, input().split())))[::-1]
+l = list(map(int, input().split()))
 
-s = 0
-for i in range(n):
-    s += l_a[i] * l_b[i]
+s = set(l)
 
-print(s)
+print(*sorted(s))

3e1ad799617815fc386600fc5a9b8b17db97135a
BoseongGeum <rmaqhtjd8364@naver.com>
2023-12-05 23:42:31 +0900

    bj231205

--- baekjoon470.py
+++ baekjoon470.py
@@ -7,6 +7,6 @@ for _ in range(n-1):
     s = input().rstrip()
     for i in range(len(result)):
         if result[i] != s[i]:
-            result[i] == '?'
+            result = result[:i] + '?' + result[i+1:]
 
 print(result)

0a0660bcebe97d94a8c8e90550b98315c8af89cb
BoseongGeum <rmaqhtjd8364@naver.com>
2024-01-01 16:28:28 +0900

    bj240101

---
+++ baekjoon492.py
@@ -0,0 +1,7 @@
+import sys
+input = sys.stdin.readline
+
+t = int(input())
+for _ in range(t):
+    a, b, c, d, e = map(int, input().split())
+    print(f'${a*350.34+b*230.90+c*190.55+d*125.30+e*180.90:.2f}')

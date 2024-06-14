1bfd5b77f4e4d3450f9bcf4f50c9dcebbc3d090c
BoseongGeum <rmaqhtjd8364@naver.com>
2024-06-14 17:10:44 +0900

    prgrmmrs38

--- test38.py
+++ test38.py
@@ -1,36 +1,23 @@
 from collections import defaultdict
 
 def solution(tickets):
-    totalCount = len(tickets)
-    visited = defaultdict(list)
-    for k, v in tickets:
-        visited[k].append(v)
+
+    graph = defaultdict(list)
+    for a,b in tickets:
+        graph[a].append(b)
+
+    for key in graph.keys():
+        graph[key].sort(reverse=True)
+
     route = []
-    
-    def dfs(city, count):
-        route.append(city)
-        temp = []
 
-        if len(route) > totalCount+1:
-            return
-        
-        for i in range(len(tickets)):
-            if tickets[i][0] == city and tickets[i][1] in visited[city]:
-                temp.append(tickets[i][1])
-        temp.sort()
+    def dfs(node):
+        while graph[node]:
+            dfs(graph[node].pop())
+        route.append(node)
 
-        if count != totalCount and temp == []:
-            route.pop()
-            for ticket in tickets:
-                visited[ticket[0]].append(ticket[1])
-        print(visited)
-        for nextCity in temp:
-            visited[city].remove(nextCity)
-            dfs(nextCity, count+1)
+    dfs("ICN")
+    return route[::-1]
 
-
-    dfs("ICN", 0)
-           
-    return route[:totalCount+1]
-
-print(solution([["ICN", "AAA"], ["ICN", "CCC"], ["CCC", "DDD"], ["AAA", "BBB"], ["AAA", "BBB"], ["DDD", "ICN"], ["BBB", "AAA"]]))
+print(solution([["ICN", "JFK"], ["HND", "IAD"], ["JFK", "HND"]])) # ["ICN", "JFK", "HND", "IAD"]
+print(solution([["ICN", "SFO"], ["ICN", "ATL"], ["SFO", "ATL"], ["ATL", "ICN"], ["ATL", "SFO"]])) # ["ICN", "ATL", "ICN", "SFO", "ATL", "SFO"]

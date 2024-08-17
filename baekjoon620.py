bf644ca6a00e6ee2b9a2da3103c2e233069acbab
BoseongGeum <rmaqhtjd8364@naver.com>
2024-08-17 14:29:58 +0900

    bj240817

---
+++ baekjoon620.py
@@ -0,0 +1,19 @@
+import sys
+input = sys.stdin.readline
+from heapq import *
+
+n = int(input())
+
+heap = []
+
+for _ in range(n):
+    i = int(input())
+
+    if i == 0:
+        if not heap:
+            print(0)
+        else:
+            print(heappop(heap))
+
+    else:
+        heappush(heap, i)

17006e51b9d34d514441031632b059e6aa3c14da
BoseongGeum <rmaqhtjd8364@naver.com>
2024-08-27 18:26:59 +0900

    bj240827

---
+++ baekjoon623.py
@@ -0,0 +1,28 @@
+import sys
+input = sys.stdin.readline
+
+count = {i:0 for i in range(10)}
+digit = 0
+
+n = int(input())
+
+for i in range(10)[::-1]:
+    share = n // (10 ** i)
+    n = n % (10 ** i)
+
+    if share == 0:
+        continue
+
+    print(share, n)
+
+    digit = max(digit, i)
+
+    for j in range(share):
+        if digit == i and j == 0:
+            continue
+        
+        count[j] += 10 ** i + share * (i-1)
+
+    count[share] += n + 1
+
+    print(*count.values())
